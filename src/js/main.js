181c5f4e4fce436f7acc8c5991a25f3f33431c5a
monnard-guillaume <monnard.guillaume@gmail.com>
2026-04-27 14:10:07 +0200

    import de threejs, orbitcontrols ainsi que gsap et scrolltrigger

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
